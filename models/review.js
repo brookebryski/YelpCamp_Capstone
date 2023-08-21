39389b543a58558cb48918f037f5694bf921f382
BrookeBryski <brookeabryski@gmail.com>
2023-08-21 12:10:19 -0700

    review model, associations, and form

---
+++ models/review.js
@@ -0,0 +1,9 @@
+const mongoose = ('mongoose');
+const { Schema } = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    body: String, 
+    rating: Number
+})
+
+module.exports = mongoose.model('Review', reviewSchema);
