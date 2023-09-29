0aba6807151f2ff5ed94349836b6768fe08ac743
BrookeBryski <brookeabryski@gmail.com>
2023-09-29 13:52:32 -0700

    deletion authorization;

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '6515e3d7fb79c24124ac4720',
+            author: '651738375cd1e14814d5ef2f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`, 
             image: 'https://source.unsplash.com/collection/483251',
